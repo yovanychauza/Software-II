8822b4cbb0d696f1c1ea857d2b34f5d689f797b7
Oscar Chauza <oscaryovany90@gmail.com>
2022-05-26 08:24:27 -0500

    deleteAll

--- src/main/java/co/edu/unicauca/asae/proyecto/maestriacomputacion/controllers/EstadoController.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unicauca.asae.proyecto.maestriacomputacion.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/estado")
-public class EstadoController {
-}
